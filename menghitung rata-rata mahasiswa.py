35bead00a20c6fa1067b9f65f0188b93254b027d
sukmahandayani0707-oss <sukmahandayani0707@gmail.com>
2026-04-11 20:58:33 +0700

    menghitung rata-rata mahasiswa.py

--- menghitung rata-rata mahasiswa.py
+++ menghitung rata-rata mahasiswa.py
@@ -1,19 +1,37 @@
-nama = ["Asep", "Budi", "Cecep"]
+mahasiswa = {
+    "10121001": "Asep",
+    "10121002": "Budi",
+    "10121003": "Cecep"
+}
 
+nilai = {
+    "10121001": [50, 70, 40, 80],
+    "10121002": [78, 78, 80, 65],
+    "10121003": [57, 88, 67, 69]
+}
 
-nilai = [
-    [50, 70, 40, 80],   # Asep
-    [78, 78, 80, 65],   # Budi
-    [57, 88, 67, 69]    # Cecep
-]
+mata_kuliah = ["MK1", "MK2", "MK3", "MK4"]
+rata_rata_mahasiswa = {}
+for nim, daftar_nilai in nilai.items():
+    rata_rata = sum(daftar_nilai) / len(daftar_nilai)
+    rata_rata_mahasiswa[nim] = rata_rata
 
-rata = [sum(n) / len(n) for n in nilai]
-idx_terbaik = rata.index(max(rata))
+nim_terpintar = max(rata_rata_mahasiswa, key=rata_rata_mahasiswa.get)
+nilai_terpintar = rata_rata_mahasiswa[nim_terpintar]
 
-print(f"Mahasiswa Terpintar: {nama[idx_terbaik]} ({rata[idx_terbaik]:.2f})")
+jumlah_mk = len(mata_kuliah)
+rata_rata_mk = [0] * jumlah_mk
 
+for daftar_nilai in nilai.values():
+    for i in range(jumlah_mk):
+        rata_rata_mk[i] += daftar_nilai[i]
 
-rata_mk = [sum(n[i] for n in nilai) / len(nilai) for i in range(len(nilai[0]))]
-idx_mk = rata_mk.index(min(rata_mk))
+for i in range(jumlah_mk):
+    rata_rata_mk[i] /= len(nilai)
 
-print(f"Mata Kuliah Nilai Terkecil: MK{idx_mk+1} ({rata_mk[idx_mk]:.2f})")
+indeks_terkecil = rata_rata_mk.index(min(rata_rata_mk))
+mk_terkecil = mata_kuliah[indeks_terkecil]
+nilai_terkecil = rata_rata_mk[indeks_terkecil]
+
+print(f"Mahasiswa Terpintar : {mahasiswa[nim_terpintar]} (Nilai : {nilai_terpintar:.2f})")
+print(f"Mata Kuliah Nilai Terkecil : {mk_terkecil} (Nilai : {nilai_terkecil:.2f})")
